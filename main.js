222e4006f25464220897ba3bdf17170ca084f66c
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-07 23:45:05 +0200

    done!

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 'use strict'
 
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana - con massimo due decimali, per indicare centesimi sul prezzo.
-// Questo richiederà un minimo di ricerca.
-// Il risultato andrà visualizzato in un apposito elemento <p> del  codice HTML.
-
 const trip = parseInt(Number(prompt('Quanti Km devi fare?')));
 const age = parseInt(Number(prompt('Quanti anni hai?')));
 
 if (isNaN(trip) || isNaN(age)) {
     alert(`
-I valori inseriti non sono numerici.
-Inserisci valori numerici per proseguire`
+I valori inseriti non sono validi.
+Inserisci valori numerici per calcolare il prezzo del biglietto`
     )
 }
 
@@ -29,14 +20,19 @@ const retiredTicket = Number.parseFloat((trip * retiredPrice).toFixed(2))
 const ticket = Number.parseFloat((trip * price).toFixed(2))
 
 if (age<18) {
-    console.log(`il tuo biglietto costa ${underAgeTicket}€`)
+    // console.log = (`il tuo biglietto costa ${underAgeTicket}€ ed è scontato del ${underAgeDiscount}%`)
     // console.log(typeof(underAgeTicket))
+    document.getElementById('price').innerHTML=underAgeTicket+'€'
+    document.getElementById('discount').innerHTML= 'Sconto: ' + underAgeDiscount + '%'
 }
 else if (age >= 65) {
-    console.log(`il tuo biglietto costa ${retiredTicket}€`)
-        // console.log(typeof(retiredTicket))
+    // console.log(`il tuo biglietto costa ${retiredTicket}€ ed è scontato del ${retiredDiscount}%`)
+    // console.log(typeof(retiredTicket))
+    document.getElementById('price').innerHTML=retiredTicket+'€'
+    document.getElementById('discount').innerHTML= 'Sconto: ' + retiredDiscount + '%'
 }
 else {
-    console.log(`il tuo biglietto costa ${ticket}€`)
+    // console.log(`il tuo biglietto costa ${ticket}€`)
     // console.log(typeof(ticket))
+    document.getElementById('price').innerHTML=ticket+'€'
 }
